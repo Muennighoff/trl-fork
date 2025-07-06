f6d9c75936896b9704354bde78a02f85e8b06f61
Muennighoff <n.muennighoff@gmail.com>
2025-07-06 02:09:03 +0000

    t

--- trl/trainer/grpo_trainer.py
+++ trl/trainer/grpo_trainer.py
@@ -1031,7 +1031,8 @@ class GRPOTrainer(Trainer):
                 else:
                     completion_ids = [None] * len(all_prompts_text)
                     import time
-                    time.sleep(3600) # Wait for 60 min # for 128x off
+                    # time.sleep(3600) # Wait for 60 min # for 128x off
+                    time.sleep(4200) # Wait for 70 min # for 128x off
                     # time.sleep(5400) # Wait for 90 min # for 256x off
                 # Broadcast the completions from the main process to all processes, ensuring each process receives its
                 # corresponding slice.
